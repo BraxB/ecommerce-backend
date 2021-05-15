4998ab5c86d4e4c5f95e8a498685ac70cafdcedd
BraxB <braxb92@gmail.com>
2021-05-15 17:51:58 -0400

    fixed issue with circular dependency

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-const { Category } = require('.');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -33,22 +32,22 @@ Product.init(
       defaultValue: 10,
       validate: {
         isNumeric: true,
-      }
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Category',
-        key: 'id',
-      }
-    }
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
